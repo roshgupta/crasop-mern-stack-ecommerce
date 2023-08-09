2ba3efef4a4b7b48358fff767bc7eb1be81cfc35
roshgupta <gpta.duke.rohan.1219@gmail.com>
2023-08-09 15:21:38 +0530

    frontend:redux setup redux and added api call and stored product data

--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -1,17 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
-
+  loading: false,
+  products: [],
+  error: null
 }
+export const listProducts = createAsyncThunk(
+  'product/listProducts',
+  async () => {
+    const { data } = await axios.get('/api/products')
+    return data
+  }
+)
 
 export const productSlice = createSlice({
   name: 'product',
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(listProducts.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(listProducts.fulfilled, (state, action) => {
+      state.loading = false
+      state.products = action.payload
+    })
+    builder.addCase(listProducts.rejected, (state, action) => {
+      state.loading = false
+      state.error = action.error.message
+    })
   }
 })
 
-export const { } = productSlice.actions
+
 
 export default productSlice.reducer
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import { listProducts } from '../redux/productSlice'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch()
+  const product = useSelector((state) => state.product)
+  const { loading, error, products } = product
+
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts();
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (<p>Loading... </p>) : error ? <p>{error}</p> : (
+        <Row>
+          {products.map(product => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+
 
     </>
   )
