3eadbee095c777b15a861c420f619161e74133bf
roshgupta <gpta.duke.rohan.1219@gmail.com>
2023-08-09 16:08:39 +0530

    frontend: added loader and alert component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../redux/productSlice'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -17,7 +19,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? (<p>Loading... </p>) : error ? <p>{error}</p> : (
+      {loading ? (<Loader />) : error ? <Message variant='danger'>{error}</Message> : (
         <Row>
           {products.map(product => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
